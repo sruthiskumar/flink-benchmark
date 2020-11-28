c997e1440b29591a0ea362e931af0a2eeca96d67
Sruthi Sree Kumar <sruthiskumar92@gmail.com>
2020-11-28 16:18:13 +0000

    updated processing script

--- logProcessingNDB.py
+++ logProcessingNDB.py
@@ -2,20 +2,37 @@ import sys
 import pandas as pd
 
 def main(argv):
-  outname = argv[1] + '.csv'
+  outnameread = argv[1] + '-read.csv'
+  outnamewrite = argv[1] + '-write.csv'
   with open(str(argv[1])+'.log') as f:
     txt = f.readlines()
     valueRead = []
     valueWrite = []
     avgvalueRead = []
     avgvalueWrite = []
-    time = []
+    timeRead = []
+    timeWrite = []
+    keyRead = []
+    keyWrite = []
+    keySet = set()
+    cachehit = 0
+    cachemiss = 0
 
   for i in range(len(txt)):
     line = txt[i].strip()
     if 'RunTime for NDB Value State Read' in line:
-      valueRead.append(line.split(' ')[13])
-      time.append(line[0:23])
+      valueRead.append(line.split(' ')[16])
+      timeRead.append(line[0:23])
+      keyRead.append(line.split(' ')[14])
+      keySet.add(line.split(' ')[14])
+    elif 'RunTime for NDB Value State Write' in line:
+      valueWrite.append(line.split(' ')[16])
+      timeWrite.append(line[0:23])
+      keyWrite.append(line.split(' ')[14])
+    elif 'Cache miss for Value State' in line:
+      cachemiss += 1
+    elif 'Cache hit for Value State' in line:
+      cachehit += 1
 #     elif 'RunTime for NDB Value State Write' in line:
 #       valueWrite.append(line.split(' ')[13])
 #     elif 'AverageTime for NDB Value State Read' in line:
@@ -24,14 +41,20 @@ def main(argv):
 #       avgvalueWrite.append(line.split(' ')[13])
 
 
-  maxlen = max([len(valueRead), len(valueWrite), len(avgvalueRead), len(avgvalueWrite)])
+#   maxlen = max([len(valueRead), len(valueWrite), len(avgvalueRead), len(avgvalueWrite)])
 #   df = pd.DataFrame([(range(maxlen)), valueRead, valueWrite, avgvalueRead, avgvalueWrite])
-  df = pd.DataFrame([time, valueRead])
-  df = df.T
+  dfRead = pd.DataFrame([timeRead, valueRead, keyRead])
+  dfWrite = pd.DataFrame([timeWrite, valueWrite, keyWrite])
+  dfRead = dfRead.T
+  dfWrite = dfWrite.T
+  print("Total Keys %s", len(keySet))
+  print("Cache Hit %s", cachehit)
+  print("Cache Miss %s", cachemiss)
 #   df.columns=['Counter','Read','Write', 'Read Average(Rolling)', 'Write Average(Rolling)']
-  df.columns=['Time','Read']
-  df.to_csv(outname, index=False)
-
+  dfRead.columns=['Time','Read', 'Key']
+  dfWrite.columns=['Time','Write', 'Key']
+  dfRead.to_csv(outnameread, index=False)
+  dfWrite.to_csv(outnamewrite, index=False)
 
 if __name__ == "__main__":
    main(sys.argv)
--- logProcessingRocksDB.py
+++ logProcessingRocksDB.py
@@ -2,40 +2,59 @@ import sys
 import pandas as pd
 
 def main(argv):
-  outname = argv[1] + '.csv'
+  outnameread = argv[1] + '-read.csv'
+  outnamewrite = argv[1] + '-write.csv'
   with open(str(argv[1])+'.log') as f:
     txt = f.readlines()
     valueRead = []
     valueWrite = []
     avgvalueRead = []
     avgvalueWrite = []
-    counter = []
+    timeRead = []
+    timeWrite = []
+    keyRead = []
+    keyWrite = []
+    keySet = set()
+    cachehit = 0
+    cachemiss = 0
 
   for i in range(len(txt)):
     line = txt[i].strip()
-
-
-#     try:
-
-#     except:
-#         print(i)
-#         print(line)
-
     if 'RunTime for RocksDB Value State Read' in line:
-      valueRead.append(line.split(' ')[13])
+      valueRead.append(line.split(' ')[16])
+      timeRead.append(line[0:23])
+      keyRead.append(line.split(' ')[14])
+      keySet.add(line.split(' ')[14])
     elif 'RunTime for RocksDB Value State Write' in line:
-      valueWrite.append(line.split(' ')[13])
-    elif 'AverageTime for RocksDB Value State Read' in line:
-      avgvalueRead.append(line.split(' ')[13])
-    elif 'AverageTime for RocksDB Value State Write' in line:
-      avgvalueWrite.append(line.split(' ')[13])
+      valueWrite.append(line.split(' ')[16])
+      timeWrite.append(line[0:23])
+      keyWrite.append(line.split(' ')[14])
+    elif 'Cache miss for' in line:
+      cachemiss += 1
+    elif 'Cache hit for' in line:
+      cachehit += 1
+#     elif 'RunTime for NDB Value State Write' in line:
+#       valueWrite.append(line.split(' ')[13])
+#     elif 'AverageTime for NDB Value State Read' in line:
+#       avgvalueRead.append(line.split(' ')[13])
+#     elif 'AverageTime for NDB Value State Write' in line:
+#       avgvalueWrite.append(line.split(' ')[13])
 
-  maxlen = max([len(valueRead), len(valueWrite), len(avgvalueRead), len(avgvalueWrite)])
-  df = pd.DataFrame([(range(maxlen)), valueRead, valueWrite, avgvalueRead, avgvalueWrite])
-  df = df.T
-  df.columns=['Counter','Read','Write', 'Read Average(Rolling)', 'Write Average(Rolling)']
-  df.to_csv(outname, index=False)
 
+#   maxlen = max([len(valueRead), len(valueWrite), len(avgvalueRead), len(avgvalueWrite)])
+#   df = pd.DataFrame([(range(maxlen)), valueRead, valueWrite, avgvalueRead, avgvalueWrite])
+  dfRead = pd.DataFrame([timeRead, valueRead, keyRead])
+  dfWrite = pd.DataFrame([timeWrite, valueWrite, keyWrite])
+  dfRead = dfRead.T
+  dfWrite = dfWrite.T
+  print("Total Keys %s", len(keySet))
+  print("Cache Hit %s", cachehit)
+  print("Cache Miss %s", cachemiss)
+#   df.columns=['Counter','Read','Write', 'Read Average(Rolling)', 'Write Average(Rolling)']
+  dfRead.columns=['Time','Read', 'Key']
+  dfWrite.columns=['Time','Write', 'Key']
+  dfRead.to_csv(outnameread, index=False)
+  dfWrite.to_csv(outnamewrite, index=False)
 
 if __name__ == "__main__":
    main(sys.argv)
